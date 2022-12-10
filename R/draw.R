157eae329b61f9aac6fedfada1f55d415293ea3a
Matt Dray <mwdray@gmail.com>
2022-12-10 14:03:19 +0000

    Improve comments, correct matrix inversion, reduce script count

--- R/draw.R
+++ R/draw.R
@@ -38,25 +38,158 @@ draw_pixels <- function(m, colours = NULL, ...) {
     )
   }
 
+  # Random colour palette if none provided
   if (is.null(colours)) {
+
+    warning(
+      "Applying random colours. Set arg 'colours' for your own palette.",
+      call. = FALSE
+    )
+
     colours <- sample(
       grDevices::colours(),
       length(unique(as.numeric(m)))
     )
+
   }
 
-  m_rev <- m[, seq(ncol(m), 1)]
-
-  par_orig <- par(mar = rep(0, 4))
+  par_start <- par(mar = rep(0, 4))
 
   graphics::image(
-    m_rev,
+    t(mat[nrow(mat):1, ]),  # reverse matrix rows and transpose
     col = colours,
-    xaxt = "n",
-    yaxt = "n",
+    axes = FALSE,
+    xlab = "",
+    ylab = "",
     ...
   )
 
-  on.exit(par(par_orig))
+  on.exit(par(par_start))
+
+}
+
+#' Create Frames of a Pixel Animation
+#'
+#' Opens a new interactive plotting canvas with a grid of clickable squares
+#' ('pixels'). When finished, the user is prompted to provide another.
+#'
+#' @param ... Parameters to pass to \code{\link{click_pixels}}.
+#'
+#' @return A list of matrices.
+#'
+#' @export
+#'
+#' @examples \dontrun{m <- frame_pixels(n_rows = 8, n_cols = 8, n_states = 3)}
+frame_pixels <- function(...) {
+
+  m_list <- list()
+
+  m <- click_pixels(...)
+
+  m_list[[1]] <- m
+
+  repeat {
+
+    answer <- readline("Add a frame? y/n: ")
+
+    if (substr(answer, 1, 1) == "y") {
+      m <- edit_pixels(m)
+      m_list <- append(m_list, list(m))
+      message("Current frame count: ", length(m_list))
+    }
+
+    if (substr(answer, 1, 1) == "n") {
+      message("Final frame count: ", length(m_list))
+      break
+    }
+
+  }
+
+  return(m_list)
+
+}
+
+#' Write Frames of a Pixel Animation to GIF
+#'
+#' Draws each list element (matrices) from the output of \code{\link{frame_pixels}}
+#' and writes it to an animated gif. Requires user to install the 'gifski'
+#' package.
+#'
+#' @param frames A list of matrices preferably produced by \code{\link{frame_pixels}}.
+#'     Each is a matrix with the same dimension and possible values that
+#'     represents a single frame of an animation.
+#' @param colours A character vector of named colours. One for each value in m,
+#'     the provided matrix. The order you provide the colours matches the order
+#'     of the values in the matrix, from 0 to n. Defaults to NULL, which means
+#'     random colours will be selected on your behalf.
+#' @param file A filepath expressed as a character vector, ending with
+#'     extension '.gif'. This is where the output animation will be written.
+#' @param ...  Parameters to pass to \code{\link[gifski:save_gif]}.
+#'
+#' @return Nothing.
+#'
+#' @export
+#'
+#' @examples \dontrun{
+#' # Create interactively a list of matrices, i.e. frames of an animation
+#' my_frames <- frame_pixels(n_rows = 8, n_cols = 8, n_states = 3)
+#'
+#' # Write
+#' gif_pixels(
+#'   frames = my_frames,
+#'   colours = c("black", "blue", "green"),  # one colour per matrix value
+#'   tempfile("example", ".gif")  # location to write GIF
+#' )
+#' }
+gif_pixels <- function(
+    frames,
+    colours = NULL,
+    file,
+    ...
+) {
+
+  if (!inherits(frames, "list")) {
+    stop(
+      "Argument 'frames' must be a list of matrices of the same size, ",
+      "preferably produced by the frame_pixels() function.",
+      call. = FALSE
+    )
+  }
+
+  if (length(seq(0, max(as.vector(m)))) < length(colours)) {
+    stop(
+      "Length of argument 'colours' should not exceed the number of unique ",
+      "pixel states in matrix 'm'.",
+      call. = FALSE
+    )
+  }
+
+  if (
+    !inherits(file, "character") |
+    length(file) != 1 |
+    substr(file, nchar(file) - 3, nchar(file)) != ".gif"
+  ) {
+    stop(
+      "Argument 'file' must be a character-string filepath ending '.gif'.",
+      call. = FALSE
+    )
+  }
+
+  gifski::save_gif(
+    invisible(
+      lapply(
+        frames,
+        function(frame) {
+          if (is.null(colours)) {
+            draw_pixels(frame)
+          } else {
+            draw_pixels(frame, colours)
+          }
+        }
+      )
+    ),
+    gif_file = file,
+    ...
+  )
 
 }
--- R/utils.R
+++ R/utils.R
@@ -1,15 +1,11 @@
-
+# Loop continuously to accept a click, update matrix values, then re-plot
 .repeat_loop <- function(m, n, g) {
 
   repeat {
 
     p <- .locate_on_grid(m)
 
-    if (is.null(p)) {
-
-      break
-
-    }
+    if (is.null(p)) break
 
     m <- .update_matrix(m, p, n)
 
@@ -17,9 +13,7 @@
 
     .plot_canvas(m, n)
 
-    if (g) {
-      .add_grid(m)
-    }
+    if (g) .add_grid(m)
 
   }
 
@@ -27,9 +21,10 @@
 
 }
 
+# Plot 'pixels' given the dimensions of the input matrix
 .plot_canvas <- function(m, n) {
 
-  graphics::par(mar = rep(1, 4))
+  par_start <- graphics::par(mar = rep(0, 4))  # set margins, save current
 
   n_rows <- nrow(m)
   n_cols <- ncol(m)
@@ -38,74 +33,101 @@
     m <- t(m[nrow(m):1, ])
   }
 
-  if (n_rows == 1) {
-    m <- t(m)
-  }
+  # Transposition and reversal required with pixel dimension of 1
+  if (n_rows == 1) m <- t(m)
+  if (n_cols == 1) m <- t(rev(m))
 
-  if (n_cols == 1) {
-    m <- t(rev(m))
-  }
-
+  # Generate greyscale colour palette, darker for higher values
   pal <- grDevices::colorRampPalette(c("white", "black"))
 
   graphics::image(
     m,
     zlim = c(0, n),
     col  = pal(n + 1),
+    axes = FALSE,
     xlab = "",
-    ylab = "",
-    axes = FALSE
+    ylab = ""
   )
+
+  on.exit(par(par_start))  # revert to user's original settings
 
 }
 
+# Draw a grid over the plotted matrix to differentiate 'pixels'
 .add_grid <- function(m) {
 
   x_n <- ncol(m)
+
+  if (x_n == 1) {
+    x_lines <- c(-1, 1)  # boundaries are -1,1 if only one pixel on this axis
+  }
+
   if (x_n > 1) {
-    x_unit  <- 1 / (x_n - 1)
-    x_lines <- seq(0 - x_unit - (x_unit / 2), 1 + x_unit + (x_unit / 2), x_unit)
-  } else if (x_n == 1) {
-    x_lines <- c(-1, 1)
+
+    x_unit  <- 1 / (x_n - 1)  # width of one pixel
+
+    x_lines <- seq(  # min/max pixel centres at -1,1, but there's 'overhang'
+      0 - x_unit - (x_unit / 2),  # add half a pixel overhang at min
+      1 + x_unit + (x_unit / 2),  # add half a pixel overhang at max
+      x_unit
+    )
+
   }
 
   y_n <- nrow(m)
-  if (y_n > 1) {
-    y_unit  <- 1 / (y_n - 1)
-    y_lines <- seq(0 - y_unit - (y_unit / 2), 1 + y_unit + (y_unit / 2), y_unit)
-  } else if (y_n == 1) {
+
+  if (y_n == 1) {
     y_lines <- c(-1, 1)
   }
 
+  if (y_n > 1) {
+
+    y_unit  <- 1 / (y_n - 1)
+
+    y_lines <- seq(
+      0 - y_unit - (y_unit / 2),
+      1 + y_unit + (y_unit / 2),
+      y_unit
+    )
+
+  }
+
+  # Draw gridlines to plot
   graphics::abline(v = x_lines)
   graphics::abline(h = y_lines)
 
 }
 
+# Get XY location of plotted 'pixel' that's been clicked by the user
 .locate_on_grid <- function(m) {
 
-  x_n    <- ncol(m)
-  x_unit <- 1 / (x_n - 1)
-  x_mids <- seq(0, 1, x_unit)
+  # Pixel centres, x axis
+  x_n    <- ncol(m)            # number of pixels in the x dimension
+  x_unit <- 1 / (x_n - 1)      # x width of pixels
+  x_mids <- seq(0, 1, x_unit)  # full set of pixel centres on x axes
 
+  # Pixel centres, y axis
   y_n    <- nrow(m)
   y_unit <- 1 / (y_n - 1)
   y_mids <- seq(0, 1, y_unit)
 
-  p <- graphics::locator(1)
+  p <- graphics::locator(1)  # prompt for interactive click
 
-  if (length(p) == 0) {
+  if (length(p) == 0) {  # if early escape from locator()
     return(NULL)
   }
 
+  # Calculate distances xy from clicked point to pixel centres
   x_diffs <- abs(p$x - x_mids)
   y_diffs <- rev(abs(p$y - y_mids))
 
+  # Identify pixel closest to click
   p <- list(
     x = which.min(x_diffs),
     y = which.min(y_diffs)
   )
 
+  #
   if (length(p$x) == 0) p$x <- 1
   if (length(p$y) == 0) p$y <- 1
 
@@ -113,17 +135,18 @@
 
 }
 
-.update_matrix <- function(m, p, n) {
+# Increment the value of the matrix that corresponds to the clicked 'pixel'
+.update_matrix <- function(m, p, n) {  # matrix, pixel coordinates, n_states
 
-  states <- seq(0L, n - 1L)
-  current_state <- m[p$y, p$x]
-  next_state <- current_state + 1L
+  states <- seq(0L, n - 1L)         # available pixel state values (0, 1, ...)
+  current_state <- m[p$y, p$x]      # current state value of pixel (e.g. 0)
+  next_state <- current_state + 1L  # next consecutive state value (e.g. 1)
 
   if (next_state > n - 1L) {
-    next_state <- states[1]
+    next_state <- states[1]  # wrap from last to first state value (e.g. 0, 1, 0)
   }
 
-  m[p$y, p$x] <- next_state
+  m[p$y, p$x] <- next_state  # update the state value in the matrix for that pixel
 
   return(m)
 
